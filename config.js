cbc77572d5c9887268802095c0c6c09a077883e1
12hadi <30751724+12hadi@users.noreply.github.com>
2023-07-14 19:30:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
-global.mail = 'support@tioprm.my.id' 
-global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6282288275146']  
+global.mods = ['6282288275146'] 
+global.prems = ['6282288275146']
+global.nameowner = 'Putra'
+global.numberowner = '6282288275146' 
+global.mail = 'gmail.com' 
+global.gc = 'https://chat.whatsapp.com/'
+global.instagram = 'https://instagram.com/'
+global.wm = '© Putra'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -14,17 +14,17 @@ global.packname = 'Made With'
 global.author = 'Bot WhatsApp'
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'ZuPr3Trv' 
 //Daftar terlebih dahulu https://api.botcahx.live
 
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = 'YpSun4Uv'
 //Daftar terlebih dahulu https://api.betabotz.org 
 
 global.APIs = {   
   btc: 'https://api.botcahx.live'
 }
 global.APIKeys = { 
-  'https://api.botcahx.live': 'Apikey' 
+  'https://api.botcahx.live': 'ZuPr3Trv' 
 }
 
 let fs = require('fs')
